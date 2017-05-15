f6a9242f43d7efe10fbb6e4035ef67d7127f1308
Raghava Juvvaji <raghava.juvvaji@Raghava.local>
2017-05-15 13:38:44 +0530

    Implemented add method in cache systems

--- cachesystems/src/main/java/com/sideprojects/cachesystems/CacheSystem.java
+++ cachesystems/src/main/java/com/sideprojects/cachesystems/CacheSystem.java
@@ -1,30 +1,27 @@
+package com.sideprojects.cachesystems;
 
 import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class CacheSystem
-{
-    private ConcurrentHashMap<String,Object> cache = null;
-    private TreeMap<Long,ArrayList<String>> expiryIndex = null;
+public class CacheSystem {
+	private ConcurrentHashMap<String, Object> cache = null;
+	private TreeMap<Long, ArrayList<String>> expiryIndex = null;
 
-	CacheSystem()
-	{
-      cache = new ConcurrentHashMap<String,Object>();
-      expiryIndex = new TreeMap<Long,ArrayList<String>>();
+	CacheSystem() {
+		cache = new ConcurrentHashMap<String, Object>();
+		expiryIndex = new TreeMap<Long, ArrayList<String>>();
 	}
 
-	public void add(String key,Object object,long inputTTL)
-	{
-      long timeToLive = System.currentTimeInMills() + (inputTTL * 1000);
-      cache.put(key,object);
+	public void add(String key, Object object, long inputTTL) {
+		long timeToLive = System.currentTimeMillis() + (inputTTL * 1000);
+		cache.put(key, object);
 
-      ArrayList<String> keys = expiryIndex.get(timeToLive);
-      if(keys == null)
-      {
-      	keys = new ArrayList<String>();
-      }
-      keys.add(key);
-      expiryIndex.put(timeToLive,keys);
+		ArrayList<String> keys = expiryIndex.get(timeToLive);
+		if (keys == null) {
+			keys = new ArrayList<String>();
+		}
+		keys.add(key);
+		expiryIndex.put(timeToLive, keys);
 	}
 }
