49718dd6848a5a3efb1fd36203091a3188e8e7e0
Raghava2016 <raghava.juvvaji@gmail.com>
2017-05-17 10:06:53 +0530

    Implemented Cleanup Thread

--- cachesystems/src/main/java/com/sideprojects/cachesystems/util/CacheCleaner.java
+++ cachesystems/src/main/java/com/sideprojects/cachesystems/util/CacheCleaner.java
@@ -1,5 +1,51 @@
 package com.sideprojects.cachesystems.util;
 
-public class CacheCleaner {
+public class CacheCleaner extends Thread {
+	ConcurrentHashMap<String,ValueObj> cache = null;
+	TreeMap<Long,ArrayList<String>> expiryIndex = null;
+	Map.Entry<Long,ArrayList<String>> indexElement = null;
 
+	int cleanUpFrequency = 1;
+
+	CacheCleaner(ConcurrentHashMap<String,ValueObj> cache,TreeMap<Long,ArrayList<String>> expiryIndex,int cleanUpFrequency)
+	{ 
+       this.cache = cache;
+       this.expiryIndex = expiryIndex;
+       this.cleanUpFrequency = cleanUpFrequency;
+	}
+
+	public void run()
+	{
+	  long currentTime = null;
+      while(true)
+      {
+         currentTime = System.currentTimeMills();
+         indexElement = cache.floorEntry(currentTime);
+
+         while(indexElement != null)
+         {
+         	Iterator<String> iterator = indexElement.getValue().iterator();
+
+         	while(iterator.hasNext())
+         	{
+              String key = iterator.next();
+              cache.remove(key);
+         	}
+         	expiryIndex.remove(currentTime);
+            indexElement = cache.floorEntry(currentTime);
+         }
+
+         try
+         {
+           synchronized(this)
+           {
+           	wait(cleanUpFrequency*30000);
+           }
+         }
+         catch(InterruptedException e)
+         {
+            e.printStackTrace();
+         }
+      }
+	}
 }
--- cachesystems/src/main/java/com/sideprojects/cachesystems/util/ValueObj.java
+++ cachesystems/src/main/java/com/sideprojects/cachesystems/util/ValueObj.java
@@ -1,5 +1,17 @@
 package com.sideprojects.cachesystems.util;
 
 public class ValueObj {
+  Object obj;
+  long expiry;
 
+  public ValueObj(Object obj,long expiry)
+  {
+    this.obj = obj;
+    this.expiry = expiry+System.currentTimeMills();
+  }
+  
+  public boolean isExpired()
+  {
+    return expiry <= System.currentTimeMills();
+  }
 }
